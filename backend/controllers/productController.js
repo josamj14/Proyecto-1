e2a8bf80a58391c5243d5b694168c2d14e751059
Brandonsaurio21001 <brandonbl2001@estudiantec.cr>
2025-05-23 17:34:44 -0600

    LOGS
    LOS DE CACHÉ

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const { getRepository } = require("../repositories/respositoryFactory");
 //const createProductIndex = require("../db/createElasticIndex");
 //const elasticClient = require("../db/elasticClient");
+const { client: redisClient, getPrefixedKey } = require("../db/redisClient");
 
 const handleResponse = (res, status, message, data = null) => {
   res.status(status).json({
@@ -17,7 +18,7 @@ const createProduct = async (req, res, next) => {
   try {
     const productRepo = getRepository("product");
     const newProductId = await productRepo.create(name, description, menu_id, price);
-
+    handleResponse(res, 201, "User created successfully", newProductId);
     if (!newProductId) {
       return handleResponse(res, 400, "No se pudo crear el producto");
     }
@@ -55,8 +56,20 @@ const createProduct = async (req, res, next) => {
 const getAllProducts = async (req, res, next) => {
   try {
     const productRepo = getRepository("product");
+    const cacheKey = getPrefixedKey("all_products");
+    const cachedData = await redisClient.get(cacheKey);
+    if (cachedData) {
+      console.log(" Cache hit para todos los productos");
+      return handleResponse(res, 200, "Users fetched successfully (cache)", JSON.parse(cachedData));
+    }
     const products = await productRepo.findAll();
     handleResponse(res, 200, "Products fetched successfully", products);
+
+    // Guardar en Redis
+    await redisClient.set(cacheKey, JSON.stringify(users), {
+      EX: 60 * 60, // Expira en 1 hora
+    });
+    
   } catch (err) {
     console.error("Error al obtener productos:", err.message);
     next(err);
